bbf0f02488f08b5ebe4d3d19ea145319a4b697b7
deepak <codewithdeepak@yahoo.com>
2020-10-22 20:01:34 +0530

    Update

--- src/app/pages/TournamentDetailsPage.js
+++ src/app/pages/TournamentDetailsPage.js
@@ -1,6 +1,6 @@
-import { Box, Paper, Typography } from "@material-ui/core";
+import { Badge, Box, Divider, Paper, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Button, Form, Card } from "react-bootstrap";
 import { MixedWidgetCustom } from "../mycomponents/mixWidgetComponent";
 import { TournamentHeader } from "../mycomponents/tournamentHeaderComponent";
 import API from "../../apiUrl.json";
@@ -50,7 +50,146 @@ const TournamentDetailsPage = (props) => {
           </Col>
 
           <Col lg={3}>
-            <Paper style={{ height: "90%" }}></Paper>
+            <Paper style={{ padding: "30px" }}>
+              <Button
+                variant="primary"
+                style={{
+                  marginTop: "20px",
+                  width: "100%",
+                  textAlign: "center",
+                  fontSize: "12px",
+                }}
+              >
+                Poker Swapping{" "}
+                <i
+                  class="fas fa-exchange-alt"
+                  style={{
+                    color: "#FFF",
+                    marginLeft: "15px",
+
+                    fontSize: "13px",
+                    float: "right",
+                    marginRight: "15px",
+                  }}
+                ></i>
+              </Button>
+              <br />
+              <Button
+                variant="primary"
+                style={{
+                  marginTop: "20px",
+                  width: "100%",
+                  textAlign: "center",
+                  fontSize: "12px",
+                }}
+              >
+                Add sponsors{" "}
+                <i
+                  class="fas fa-plus"
+                  style={{
+                    color: "#FFF",
+                    marginLeft: "15px",
+                    fontSize: "13px",
+                    float: "right",
+                    marginRight: "15px",
+                  }}
+                ></i>
+              </Button>
+              <br />
+              <Button
+                variant="primary"
+                style={{
+                  marginTop: "20px",
+                  width: "100%",
+                  textAlign: "center",
+                  fontSize: "12px",
+                }}
+              >
+                View Live{" "}
+                <i
+                  class="fas fa-tv"
+                  style={{
+                    color: "#FFF",
+                    marginLeft: "15px",
+                    fontSize: "13px",
+                    float: "right",
+                    marginRight: "15px",
+                  }}
+                ></i>
+              </Button>
+
+              <Button
+                variant="primary"
+                style={{
+                  marginTop: "20px",
+                  width: "100%",
+                  textAlign: "center",
+                  fontSize: "12px",
+                }}
+              >
+                {details.network}{" "}
+                <i
+                  class="fas fa-external-link-alt"
+                  style={{
+                    color: "#FFF",
+                    marginLeft: "15px",
+                    fontSize: "13px",
+                    float: "right",
+                    marginRight: "15px",
+                  }}
+                ></i>
+              </Button>
+              <Button
+                variant="primary"
+                style={{
+                  marginTop: "20px",
+                  width: "100%",
+                  textAlign: "center",
+                  fontSize: "12px",
+                }}
+              >
+                Add to calender{" "}
+                <i
+                  class="fas fa-calendar-check"
+                  style={{
+                    color: "#FFF",
+                    marginLeft: "15px",
+                    fontSize: "13px",
+                    float: "right",
+                    marginRight: "15px",
+                  }}
+                ></i>
+              </Button>
+              <Button
+                variant="primary"
+                style={{
+                  marginTop: "20px",
+                  width: "100%",
+                  textAlign: "center",
+                  fontSize: "12px",
+                }}
+              >
+                Share Tournament{" "}
+                <i
+                  class="fas fa-share"
+                  style={{
+                    color: "#FFF",
+                    marginLeft: "15px",
+                    fontSize: "13px",
+                    float: "right",
+                    marginRight: "15px",
+                  }}
+                ></i>
+              </Button>
+            </Paper>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={9}>
+            <Paper style={{ padding: "30px" }}>
+              <h4>Participants</h4>
+              <Divider style={{ marginTop: "15px" }} />
+            </Paper>
           </Col>
         </Row>
       </Box>
