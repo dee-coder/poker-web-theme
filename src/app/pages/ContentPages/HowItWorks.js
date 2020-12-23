6bcf59b9d1a1635d290ffc3a9a461fc8c1489eb7
deepak <codewithdeepak@yahoo.com>
2020-12-23 18:59:42 +0530

    Url replaced with endpoints

--- src/app/pages/ContentPages/HowItWorks.js
+++ src/app/pages/ContentPages/HowItWorks.js
@@ -1,20 +1,78 @@
 import { Box, Typography } from "@material-ui/core";
+import { Button } from "react-bootstrap";
 import React from "react";
+import { Image } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 import { Row } from "react-bootstrap";
+import { toAbsoluteUrl } from "../../../_metronic/_helpers";
 
 const HowItWorks = () => {
   return (
     <Box>
       <Row>
         <Col lg={12}>
-          <div className="d-flex align-items-center justify-content-center">
-            <Typography
-              variant="h4"
-              style={{ color: "white", fontWeight: "600" }}
-            >
-              How It Works
-            </Typography>
+          <div className="card">
+            <div className="card-body">
+              <Row>
+                <Col lg={6}>
+                  <div
+                    className="d-flex flex-column justify-content-start"
+                    style={{ paddingLeft: "20px" }}
+                  >
+                    <Typography
+                      variant="h1"
+                      style={{
+                        color: "black",
+                        fontWeight: "600",
+                        fontSize: "40px",
+                        marginTop: "20px",
+                      }}
+                    >
+                      How it works?
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ marginTop: "30px", color: "black" }}
+                    >
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Vestibulum ante ipsum primis in faucibus orci luctus et
+                      ultrices posuere cubilia Curae.
+                    </Typography>
+                    <div style={{ marginTop: "30px" }}>
+                      <Button
+                        variant="success"
+                        style={{
+                          height: "50px",
+                          fontSize: "14px",
+                          width: "150px",
+                        }}
+                      >
+                        Get Started
+                      </Button>
+                      <Button
+                        variant="outline-success"
+                        style={{
+                          marginLeft: "20px",
+                          height: "50px",
+                          fontSize: "14px",
+                          width: "150px",
+                        }}
+                      >
+                        Get it touch
+                      </Button>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={6}>
+                  <div className="d-flex align-items-center justify-content-center">
+                    <Image
+                      src={toAbsoluteUrl("/media/banners/how-it-works.svg")}
+                      style={{ width: "350px", height: "250px" }}
+                    />
+                  </div>
+                </Col>
+              </Row>
+            </div>
           </div>
         </Col>
       </Row>
