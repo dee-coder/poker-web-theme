e02120f548dd9f46fc3a1ee49ad3490596e3550c
deepak <codewithdeepak@yahoo.com>
2020-12-24 11:31:17 +0530

    How it works page updated

--- src/app/pages/ContentPages/HowItWorks.js
+++ src/app/pages/ContentPages/HowItWorks.js
@@ -11,7 +11,7 @@ const HowItWorks = () => {
     <Box>
       <Row>
         <Col lg={12}>
-          <div className="card">
+          <div className="card shadow p-3 mb-5 bg-white rounded">
             <div className="card-body">
               <Row>
                 <Col lg={6}>
@@ -72,6 +72,232 @@ const HowItWorks = () => {
                   </div>
                 </Col>
               </Row>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: "30px" }}>
+        <Col lg={12}>
+          <div
+            className="d-flex align-items-center justify-content-start"
+            style={{ padding: "10px" }}
+          >
+            <Typography
+              variant="button"
+              style={{
+                fontSize: "15px",
+                fontWeight: "600",
+                color: "#17a2b8",
+              }}
+            >
+              STEPS
+            </Typography>
+          </div>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: "20px" }}>
+        <Col lg={3}>
+          <div className="d-flex flex-column justify-content-center">
+            <div
+              className="card shadow d-flex justify-content-center align-items-center"
+              style={{
+                height: "50px",
+                width: "50px",
+                borderRadius: "25px",
+                background: "#17a2b8",
+                zIndex: "1000",
+                marginLeft: "20px",
+              }}
+            >
+              <span
+                style={{ fontSize: "25px", fontWeight: "900", color: "white" }}
+              >
+                1
+              </span>
+            </div>
+            <div
+              className="card"
+              style={{
+                height: "300px",
+                width: "100%",
+                marginTop: "-25px",
+                borderRadius: "12px",
+                background: "#fff",
+                zIndex: "100",
+                padding: "30px",
+              }}
+            >
+              <Typography
+                variant="body1"
+                style={{
+                  fontWeight: "600",
+                  fontSize: "20px",
+                  color: "black",
+                  marginTop: "30px",
+                }}
+              >
+                Vestibulum ante
+              </Typography>
+
+              <Typography variant="body1" style={{ marginTop: "20px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia Curae. Primis in faucibus orci luctus et
+                ultrices posuere cubilia Curae.
+              </Typography>
+            </div>
+          </div>
+        </Col>
+        <Col lg={3}>
+          <div className="d-flex flex-column justify-content-center">
+            <div
+              className="card shadow d-flex justify-content-center align-items-center"
+              style={{
+                height: "50px",
+                width: "50px",
+                borderRadius: "25px",
+                background: "#17a2b8",
+                zIndex: "1000",
+                marginLeft: "20px",
+              }}
+            >
+              <span
+                style={{ fontSize: "25px", fontWeight: "900", color: "white" }}
+              >
+                2
+              </span>
+            </div>
+            <div
+              className="card"
+              style={{
+                height: "300px",
+                width: "100%",
+                marginTop: "-25px",
+                borderRadius: "12px",
+                background: "#fff",
+                zIndex: "100",
+                padding: "30px",
+              }}
+            >
+              <Typography
+                variant="body1"
+                style={{
+                  fontWeight: "600",
+                  fontSize: "20px",
+                  color: "black",
+                  marginTop: "30px",
+                }}
+              >
+                Vestibulum ante
+              </Typography>
+              <Typography variant="body1" style={{ marginTop: "20px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia Curae. Primis in faucibus orci luctus et
+                ultrices posuere cubilia Curae.
+              </Typography>
+            </div>
+          </div>
+        </Col>
+        <Col lg={3}>
+          <div className="d-flex flex-column justify-content-center">
+            <div
+              className="card shadow d-flex justify-content-center align-items-center"
+              style={{
+                height: "50px",
+                width: "50px",
+                borderRadius: "25px",
+                background: "#17a2b8",
+                zIndex: "1000",
+                marginLeft: "20px",
+              }}
+            >
+              <span
+                style={{ fontSize: "25px", fontWeight: "900", color: "white" }}
+              >
+                3
+              </span>
+            </div>
+            <div
+              className="card"
+              style={{
+                height: "300px",
+                width: "100%",
+                marginTop: "-25px",
+                borderRadius: "12px",
+                background: "#fff",
+                zIndex: "100",
+                padding: "30px",
+              }}
+            >
+              <Typography
+                variant="body1"
+                style={{
+                  fontWeight: "600",
+                  fontSize: "20px",
+                  color: "black",
+                  marginTop: "30px",
+                }}
+              >
+                Vestibulum ante
+              </Typography>
+              <Typography variant="body1" style={{ marginTop: "20px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia Curae. Primis in faucibus orci luctus et
+                ultrices posuere cubilia Curae.
+              </Typography>
+            </div>
+          </div>
+        </Col>
+        <Col lg={3}>
+          <div className="d-flex flex-column justify-content-center">
+            <div
+              className="card shadow d-flex justify-content-center align-items-center"
+              style={{
+                height: "50px",
+                width: "50px",
+                borderRadius: "25px",
+                background: "#17a2b8",
+                zIndex: "1000",
+                marginLeft: "20px",
+              }}
+            >
+              <span
+                style={{ fontSize: "25px", fontWeight: "900", color: "white" }}
+              >
+                4
+              </span>
+            </div>
+            <div
+              className="card"
+              style={{
+                height: "300px",
+                width: "100%",
+                marginTop: "-25px",
+                borderRadius: "12px",
+                background: "#fff",
+                zIndex: "100",
+                padding: "30px",
+              }}
+            >
+              <Typography
+                variant="body1"
+                style={{
+                  fontWeight: "600",
+                  fontSize: "20px",
+                  color: "black",
+                  marginTop: "30px",
+                }}
+              >
+                Vestibulum ante
+              </Typography>
+              <Typography variant="body1" style={{ marginTop: "20px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia Curae. Primis in faucibus orci luctus et
+                ultrices posuere cubilia Curae.
+              </Typography>
             </div>
           </div>
         </Col>
